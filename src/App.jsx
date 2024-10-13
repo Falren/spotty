ad17c1e1235de98b1861c7974ebaa178bf0bbe37
Dmytro Petrov <nokuchie@gmail.com>
2024-10-13 16:39:47 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,71 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+
+function App() {
+  const [socket, setSocket] = useState(null)
+  const [messages, setMessages] = useState([])
+  const [input, setInput] = useState('') 
+  const roomId = 1
+
+  useEffect(() => {
+    const ws = new WebSocket(`ws://localhost:3000/cable?room_id=${roomId}`);
+    
+    ws.onopen = () => { 
+      const subscriptionMessage = {
+        command: 'subscribe',
+        identifier: JSON.stringify({
+          channel: 'ChatChannel',
+          chat_id: roomId
+        })
+      }
+      ws.send(JSON.stringify(subscriptionMessage))
+    };
+
+    ws.onmessage = (event) => { 
+      const messageData = JSON.parse(event.data);
+      if (messageData.type == 'ping') return;
+      if (messageData.message) {
+        const messageBody = messageData.message.body;
+        setMessages((prevMessages) => [...prevMessages, messageBody])
+       }
+    };
+
+    setSocket(ws);
+    
+    return () => { ws.close() }
+  }, [roomId]);
+
+  const sendMessage = () => {
+    if (input && socket) {
+      const chatMessage = {
+        command: 'message',
+        identifier: JSON.stringify({ channel: 'ChatChannel', chat_id: roomId }),
+        data: JSON.stringify({ message: input })
+      }
+      socket.send(JSON.stringify(chatMessage));
+      setInput('')
+    }
+  }  
+
+  return (
+    <>
+      <input 
+        type='text'
+        value={input}
+        onChange={(e) => setInput(e.target.value)}       
+      />
+      <button onClick={sendMessage}>
+        Send Message
+      </button>
+      <ul>
+        {messages.map((message, index) => (
+          <li key={index}>
+            {message}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default App;
